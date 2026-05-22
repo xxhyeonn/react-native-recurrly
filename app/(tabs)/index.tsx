b677a45ad0d87d07d0fb000a8a1fddfd573db1fa
xxhyeonn <moonjeonghyeon02@gmail.com>
2026-05-22 15:57:55 +0900

    implement fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,23 +5,15 @@ import { styled } from "react-native-css";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
 
 const SafeAreaView = styled(RNSafeAreaView);
+
 export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
-      <Link href="/onboarding" className="mt-4 rounded bg-primary text-white p-4">Go to Onboarding</Link>
-      <Link href="/(auth)/sign-in" className="mt-4 rounded bg-primary text-white p-4">Go to Sign In</Link>
-      <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white p-4">Go to Sign Up</Link>
+      <Text className="text-5xl font-sans-extrabold">Home</Text>
 
-      <Link href="/subscriptions/spotify">Go to Spotify</Link>
-
-      <Link href={{
-        pathname: "/subscriptions/[id]",
-        params: { id: "claude" }
-      }} 
-      >Claude Max Subscription</Link>
+      <Link href="/onboarding" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">Go to Onboarding</Link>
+      <Link href="/(auth)/sign-in" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">Go to Sign In</Link>
+      <Link href="/(auth)/sign-up" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">Go to Sign Up</Link>
     </SafeAreaView>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,24 @@
 import "@/global.css";
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    'sans-regular': require("@/assets/fonts/PlusJakartaSans-Regular.ttf"),
+    'sans-light': require("@/assets/fonts/PlusJakartaSans-Light.ttf"),
+    'sans-medium': require("@/assets/fonts/PlusJakartaSans-Medium.ttf"),
+    'sans-semibold': require("@/assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+    'sans-bold': require("@/assets/fonts/PlusJakartaSans-Bold.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {return null;}
+
   return <Stack screenOptions={{ headerShown: false }} />;
 }
